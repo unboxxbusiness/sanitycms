1914fcd43fbedb57342e969d7df77f44818e86fc
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-28 06:37:49 +0000

    Runtime Error
    Error: Could not parse module '[project]/node_modules/ne

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -4,7 +4,17 @@
 
 import {NextStudio} from 'next-sanity/studio'
 import config from '../../../../sanity.config'
+import {Suspense} from 'react'
+import dynamic from 'next/dynamic'
+
+const Studio = dynamic(() => import('next-sanity/studio').then((mod) => mod.NextStudio), {
+  ssr: false,
+})
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return (
+    <Suspense>
+      <Studio config={config} />
+    </Suspense>
+  )
 }
