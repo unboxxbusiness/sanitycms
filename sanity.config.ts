81e21bafdb7fc84071a2b8afb68cc8b3ba8f57b1
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-28 07:02:39 +0000

    yes

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,40 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {structureTool, type StructureResolver} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './src/sanity/schemas'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
+const singletonActions = new Set(["publish", "discardChanges", "restore"])
+const singletonTypes = new Set(["homePage"])
+
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .title('Content')
+    .items([
+      // Our singleton type has a list item with a custom child
+      S.listItem()
+        .title('Home Page')
+        .id('homePage')
+        .child(
+          // Instead of rendering a list of documents, we render a single
+          // document, specifying the `documentId` manually to ensure
+          // that we're editing the single instance of the document
+          S.document()
+            .schemaType('homePage')
+            .documentId('homePage')
+        ),
+
+      // Regular document types
+      S.documentTypeListItem('page').title('Pages'),
+
+      // The rest of our document types
+      ...S.documentTypeListItems().filter(
+        (listItem) => !['homePage', 'page'].includes(listItem.getId() as string)
+      ),
+    ])
+
 
 export default defineConfig({
   basePath: '/studio',
@@ -13,9 +43,23 @@ export default defineConfig({
   projectId,
   dataset,
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({
+    structure
+  }), visionTool()],
 
   schema: {
     types: schemaTypes,
+    // Filter out singleton types from the global “New document” menu options
+    templates: (templates) =>
+      templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
+  },
+
+  document: {
+    // For singleton types, filter out actions that are not explicitly included
+    // in the `singletonActions` list defined above
+    actions: (input, context) =>
+      singletonTypes.has(context.schemaType)
+        ? input.filter(({ action }) => action && singletonActions.has(action))
+        : input,
   },
 })
