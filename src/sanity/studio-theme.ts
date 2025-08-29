79b3cbf42ac1ea5db7f1870be4a4ecea824fc879
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-29 07:53:30 +0000

    my brand colors are these#fca311,#14213d,#e5e5e5,#000000,#ffffff not you

--- src/sanity/studio-theme.ts
+++ src/sanity/studio-theme.ts
@@ -2,49 +2,46 @@
 import { buildLegacyTheme } from 'sanity';
 
 const props = {
-    '--my-white': '#ffffff',
-    '--my-black': '#1a1a1a',
-    '--my-blue': '#4285f4',
-    '--my-red': '#db4437',
-    '--my-yellow': '#f4b400',
-    '--my-green': '#0f9d58',
-    '--amulyax-primary': 'hsl(231, 66%, 56%)',
-    '--amulyax-accent': 'hsl(282, 82%, 56%)',
-    '--amulyax-background': 'hsl(240, 10%, 96.1%)',
-    '--amulyax-foreground': 'hsl(240, 10%, 3.9%)',
-    '--amulyax-card': 'hsl(240, 10%, 96.1%)',
+    '--brand-white': '#ffffff',
+    '--brand-black': '#000000',
+    '--brand-light-gray': '#e5e5e5',
+    '--brand-dark-blue': '#14213d',
+    '--brand-orange-gold': '#fca311',
+
+    '--brand-red': '#db4437',
+    '--brand-green': '#0f9d58',
 };
 
 export const studioTheme = buildLegacyTheme({
     /* Base theme colors */
-    '--black': props['--my-black'],
-    '--white': props['--my-white'],
+    '--black': props['--brand-dark-blue'],
+    '--white': props['--brand-white'],
 
     '--gray': '#666',
     '--gray-base': '#666',
 
-    '--component-bg': props['--amulyax-background'],
-    '--component-text-color': props['--amulyax-foreground'],
+    '--component-bg': props['--brand-white'],
+    '--component-text-color': props['--brand-dark-blue'],
 
     /* Brand */
-    '--brand-primary': props['--amulyax-primary'],
+    '--brand-primary': props['--brand-orange-gold'],
 
     // Default button
     '--default-button-color': '#666',
-    '--default-button-primary-color': props['--amulyax-primary'],
-    '--default-button-success-color': props['--my-green'],
-    '--default-button-warning-color': props['--my-yellow'],
-    '--default-button-danger-color': props['--my-red'],
+    '--default-button-primary-color': props['--brand-orange-gold'],
+    '--default-button-success-color': props['--brand-green'],
+    '--default-button-warning-color': props['--brand-orange-gold'],
+    '--default-button-danger-color': props['--brand-red'],
 
     /* State */
-    '--state-info-color': props['--amulyax-primary'],
-    '--state-success-color': props['--my-green'],
-    '--state-warning-color': props['--my-yellow'],
-    '--state-danger-color': props['--my-red'],
+    '--state-info-color': props['--brand-orange-gold'],
+    '--state-success-color': props['--brand-green'],
+    '--state-warning-color': props['--brand-orange-gold'],
+    '--state-danger-color': props['--brand-red'],
 
     /* Navbar */
-    '--main-navigation-color': props['--my-black'],
-    '--main-navigation-color--inverted': props['--my-white'],
+    '--main-navigation-color': props['--brand-dark-blue'],
+    '--main-navigation-color--inverted': props['--brand-white'],
 
-    '--focus-color': props['--amulyax-accent'],
+    '--focus-color': props['--brand-orange-gold'],
 });
