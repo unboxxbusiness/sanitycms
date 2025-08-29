7d34b9798f3eb9e65c0b74190759148838f2826b
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-29 12:07:44 +0000

    no header and footer requried on studio page

---
+++ src/app/studio/layout.tsx
@@ -0,0 +1,14 @@
+// src/app/studio/layout.tsx
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
