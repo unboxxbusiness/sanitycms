41bfcb8342487c3302041fb19ba77d4d4bdd3ba8
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-30 01:01:37 +0000

    Runtime Error
    Error: Could not parse module '[project]/node_modules/ne

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,7 +1,10 @@
 // src/app/studio/[[...tool]]/page.tsx
-
 'use client'
 
+/**
+ * This route is responsible for the built-in authoring environment for Sanity Studio.
+ * All routes under /studio will be handled by this file.
+ **/
 import {NextStudio} from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 import {Suspense} from 'react'
